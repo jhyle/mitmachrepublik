c17d6e344f46eb585169d66de7b7105f657c4cf1
Leonhard Holz <marvin1444@googlemail.com>
2014-11-24 16:44:02 +0100

    implemented error logging

--- web/web.go
+++ web/web.go
@@ -27,6 +27,12 @@ type (
 		Email string
 		Pwd   string
 	}
+
+	webResult struct {
+		Status int
+		Error  error
+		JSON   interface{}
+	}
 )
 
 const (
@@ -34,6 +40,14 @@ const (
 	register_message = "Liebe/r Organisator/in von %s,\r\n\r\nvielen Dank für die Registrierung bei der MitmachRepublik. Bitte bestätige Deine Registrierung, in dem Du auf den folgenden Link klickst:\r\n\r\nhttp://dev.mitmachrepublik.de/approve/%s\r\n\r\nDas Team der MitmachRepublik"
 	password_subject = "Deine neue E-Mail-Adresse bei mitmachrepublik.de"
 	password_message = "Liebe/r Organisator/in von %s,\r\n\r\nbitte bestätige Deine neue E-Mail-Adresse, in dem Du auf den folgenden Link klickst:\r\n\r\nhttp://dev.mitmachrepublik.de/approve/%s\r\n\r\nDas Team der MitmachRepublik"
+)
+
+var (
+	resultOK         = &webResult{Status: http.StatusOK}
+	resultCreated    = &webResult{Status: http.StatusCreated}
+	resultBadRequest = &webResult{Status: http.StatusBadRequest}
+	resultNotFound   = &webResult{Status: http.StatusNotFound}
+	resultConflict   = &webResult{Status: http.StatusConflict}
 )
 
 func NewWebServer(host string, port int, tplDir, imgServer, mongoUrl, dbName string) (*WebServer, error) {
@@ -52,47 +66,72 @@ func NewWebServer(host string, port int, tplDir, imgServer, mongoUrl, dbName str
 	return &WebServer{host, port, tpls, imgServer, database, emailAccount}, nil
 }
 
+func (web *WebServer) view(tpl string, w traffic.ResponseWriter, data bson.M) *webResult {
+
+	err := web.tpls.Execute(tpl, w, data)
+	if err != nil {
+		return &webResult{Status: http.StatusInternalServerError, Error: err}
+	}
+
+	return resultOK
+}
+
+func (web *WebServer) handle(w traffic.ResponseWriter, result *webResult) {
+
+	if result.Error != nil {
+		traffic.Logger().Print(result.Error.Error())
+	}
+
+	if !w.Written() {
+		w.WriteHeader(result.Status)
+	}
+
+	if result.JSON != nil && !w.BodyWritten() {
+		w.WriteJSON(result.JSON)
+	}
+}
+
 func (web *WebServer) startPage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	err := web.tpls.Execute("start.tpl", w, nil)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+	result := func() *webResult {
+		return web.view("start.tpl", w, nil)
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) approvePage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	var err error = nil
+	result := func() *webResult {
 
-	if !bson.IsObjectIdHex(r.Param("id")) {
-		err = errors.New("Failed to read id.")
-	} else {
-		var user User
-		userId := bson.ObjectIdHex(r.Param("id"))
+		var err error = nil
 
-		err = web.database.Table("user").LoadById(userId, &user)
-		if err == nil {
-			user.Approved = true
-			_, err = web.database.Table("user").UpsertById(userId, &user)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
+		if bson.IsObjectIdHex(r.Param("id")) {
+			var user User
+			userId := bson.ObjectIdHex(r.Param("id"))
+
+			err = web.database.Table("user").LoadById(userId, &user)
+			if err == nil {
+				user.Approved = true
+				if _, err = web.database.Table("user").UpsertById(userId, &user); err != nil {
+					return &webResult{Status: http.StatusInternalServerError, Error: err}
+				}
 			}
 		}
-	}
 
-	err = web.tpls.Execute("approve.tpl", w, bson.M{"approved": err == nil})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+		return web.view("approve.tpl", w, bson.M{"approved": err == nil})
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) eventsPage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	err := web.tpls.Execute("events.tpl", w, nil)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+	result := func() *webResult {
+		return web.view("events.tpl", w, nil)
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) checkSession(r *Request) (*User, error) {
@@ -112,78 +151,82 @@ func (web *WebServer) checkSession(r *Request) (*User, error) {
 
 func (web *WebServer) adminPage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := web.checkSession((&Request{r}))
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	var events []Event
-	web.database.Table("event").Find(bson.M{"organizerid": user.Id}, &events)
+		user, err := web.checkSession((&Request{r}))
+		if err != nil {
+			return resultBadRequest
+		}
 
-	err = web.tpls.Execute("admin.tpl", w, bson.M{"user": user, "events": events})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+		var events []Event
+		err = web.database.Table("event").Find(bson.M{"organizerid": user.Id}, &events)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return web.view("admin.tpl", w, bson.M{"user": user, "events": events})
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) profilePage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := web.checkSession((&Request{r}))
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	err = web.tpls.Execute("profile.tpl", w, bson.M{"user": user})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+		user, err := web.checkSession((&Request{r}))
+		if err != nil {
+			return resultBadRequest
+		}
+
+		return web.view("profile.tpl", w, bson.M{"user": user})
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) passwordPage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := web.checkSession((&Request{r}))
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	err = web.tpls.Execute("password.tpl", w, bson.M{"user": user})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+		user, err := web.checkSession((&Request{r}))
+		if err != nil {
+			return resultBadRequest
+		}
+
+		return web.view("password.tpl", w, bson.M{"user": user})
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) eventPage(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := web.checkSession((&Request{r}))
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	var data Event
-	event := &data
-	if bson.IsObjectIdHex(r.Param("id")) {
-
-		err = web.database.Table("event").LoadById(bson.ObjectIdHex(r.Param("id")), event)
+		user, err := web.checkSession((&Request{r}))
 		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
+			return resultBadRequest
 		}
 
-		if event.OrganizerId != user.Id {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-	}
+		var data Event
+		event := &data
+		if bson.IsObjectIdHex(r.Param("id")) {
 
-	err = web.tpls.Execute("event.tpl", w, bson.M{"user": user, "event": event, "categories": CategoryOrder, "categoryIds": CategoryMap})
-	if err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+			err = web.database.Table("event").LoadById(bson.ObjectIdHex(r.Param("id")), event)
+			if err != nil {
+				return &webResult{Status: http.StatusNotFound, Error: err}
+			}
+
+			if event.OrganizerId != user.Id {
+				return resultBadRequest
+			}
+		}
+
+		return web.view("event.tpl", w, bson.M{"user": user, "event": event, "categories": CategoryOrder, "categoryIds": CategoryMap})
+	}()
+
+	web.handle(w, result)
 }
 
 func isEmpty(s string) bool {
@@ -218,24 +261,27 @@ func (web *WebServer) searchHandler(w traffic.ResponseWriter, r *traffic.Request
 
 func (web *WebServer) uploadHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	file, _, err := r.FormFile("file")
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	filename := uuid.New() + ".jpg"
-	resp, err := http.Post(web.imgServer+"/"+filename, "image/jpeg", file)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		file, _, err := r.FormFile("file")
+		if err != nil {
+			return resultBadRequest
+		}
 
-	if resp.StatusCode == http.StatusOK {
-		w.WriteJSON(filename)
-	} else {
-		w.WriteHeader(resp.StatusCode)
-	}
+		filename := uuid.New() + ".jpg"
+		resp, err := http.Post(web.imgServer+"/"+filename, "image/jpeg", file)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			return &webResult{Status: http.StatusOK, JSON: filename}
+		} else {
+			return &webResult{Status: resp.StatusCode}
+		}
+	}()
+
+	web.handle(w, result)
 }
 
 func validateUser(db Database, user *User) error {
@@ -269,237 +315,262 @@ func (web *WebServer) sendEmail(to, subject, body string) error {
 
 func (web *WebServer) registerHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := (&Request{r}).ReadUser()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	err = validateUser(web.database, user)
-	if err != nil {
-		w.WriteHeader(http.StatusConflict)
-		return
-	}
+		user, err := (&Request{r}).ReadUser()
+		if err != nil {
+			return resultBadRequest
+		}
 
-	user.Id = bson.NewObjectId()
-	_, err = web.database.Table("user").UpsertById(user.Id, user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		err = validateUser(web.database, user)
+		if err != nil {
+			return resultConflict
+		}
 
-	err = web.sendEmail(user.Email, register_subject, fmt.Sprintf(register_message, user.Addr.Name, user.Id.Hex()))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		user.Id = bson.NewObjectId()
+		_, err = web.database.Table("user").UpsertById(user.Id, user)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
 
-	id, err := web.database.CreateSession(user.Id)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		err = web.sendEmail(user.Email, register_subject, fmt.Sprintf(register_message, user.Addr.Name, user.Id.Hex()))
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
 
-	w.WriteHeader(http.StatusCreated)
-	w.WriteJSON(id)
+		id, err := web.database.CreateSession(user.Id)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return &webResult{Status: http.StatusCreated, JSON: id}
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) profileHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	request := &Request{r}
+	result := func() *webResult {
 
-	user, err := web.checkSession(request)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		request := &Request{r}
 
-	data, err := request.ReadUser()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		user, err := web.checkSession(request)
+		if err != nil {
+			return resultBadRequest
+		}
 
-	user.Addr.Name = data.Addr.Name
-	user.Image = data.Image
-	user.Descr = data.Descr
-	user.Web = data.Web
-	user.Addr.Street = data.Addr.Street
-	user.Addr.Pcode = data.Addr.Pcode
-	user.Addr.City = data.Addr.City
+		data, err := request.ReadUser()
+		if err != nil {
+			return resultBadRequest
+		}
 
-	_, err = web.database.Table("user").UpsertById(user.Id, user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+		user.Addr.Name = data.Addr.Name
+		user.Image = data.Image
+		user.Descr = data.Descr
+		user.Web = data.Web
+		user.Addr.Street = data.Addr.Street
+		user.Addr.Pcode = data.Addr.Pcode
+		user.Addr.City = data.Addr.City
+
+		_, err = web.database.Table("user").UpsertById(user.Id, user)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return resultOK
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) passwordHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	request := &Request{r}
+	result := func() *webResult {
 
-	user, err := web.checkSession(request)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		request := &Request{r}
 
-	data, err := request.ReadEmailAndPwd()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !isEmpty(data.Pwd) {
-		user.Pwd = data.Pwd
-	}
-
-	if !isEmpty(data.Email) && data.Email != user.Email {
-		user.Email = data.Email
-		user.Approved = false
-		err := web.sendEmail(user.Email, password_subject, fmt.Sprintf(password_message, user.Addr.Name, user.Id.Hex()))
+		user, err := web.checkSession(request)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			return resultBadRequest
 		}
-	}
 
-	_, err = web.database.Table("user").UpsertById(user.Id, user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+		data, err := request.ReadEmailAndPwd()
+		if err != nil {
+			return resultBadRequest
+		}
+
+		if !isEmpty(data.Pwd) {
+			user.Pwd = data.Pwd
+		}
+
+		if !isEmpty(data.Email) && data.Email != user.Email {
+			user.Email = data.Email
+			user.Approved = false
+			err := web.sendEmail(user.Email, password_subject, fmt.Sprintf(password_message, user.Addr.Name, user.Id.Hex()))
+			if err != nil {
+				return &webResult{Status: http.StatusInternalServerError, Error: err}
+			}
+		}
+
+		_, err = web.database.Table("user").UpsertById(user.Id, user)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return resultOK
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) unregisterHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := web.checkSession(&Request{r})
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	err = web.database.Table("event").Delete(bson.M{"organizerid": user.Id})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		user, err := web.checkSession(&Request{r})
+		if err != nil {
+			return resultBadRequest
+		}
 
-	err = web.database.Table("user").DeleteById(user.Id)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		err = web.database.Table("event").Delete(bson.M{"organizerid": user.Id})
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
 
+		err = web.database.Table("user").DeleteById(user.Id)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return resultOK
+	}()
+
+	web.handle(w, result)
 	web.logoutHandler(w, r)
 }
 
 func (web *WebServer) eventHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	request := &Request{r}
+	result := func() *webResult {
 
-	user, err := web.checkSession(request)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		request := &Request{r}
 
-	var data Event
-	event := &data
-	err = request.ReadJson(event)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	created := false
-	if event.Id.Valid() {
-		var oldData Event
-		oldEvent := &oldData
-		web.database.Table("event").LoadById(event.Id, oldEvent)
-		if oldEvent.OrganizerId != user.Id {
-			w.WriteHeader(http.StatusBadRequest)
-			return
+		user, err := web.checkSession(request)
+		if err != nil {
+			return resultBadRequest
 		}
-	} else {
-		created = true
-		event.Id = bson.NewObjectId()
-	}
-	event.OrganizerId = user.Id
 
-	_, err = web.database.Table("event").UpsertById(event.Id, event)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		var data Event
+		event := &data
+		err = request.ReadJson(event)
+		if err != nil {
+			return resultBadRequest
+		}
 
-	if created {
-		w.WriteHeader(http.StatusCreated)
-	}
+		created := false
+		if event.Id.Valid() {
+			var oldData Event
+			oldEvent := &oldData
+			web.database.Table("event").LoadById(event.Id, oldEvent)
+			if oldEvent.OrganizerId != user.Id {
+				return resultBadRequest
+			}
+		} else {
+			created = true
+			event.Id = bson.NewObjectId()
+		}
+		event.OrganizerId = user.Id
+
+		_, err = web.database.Table("event").UpsertById(event.Id, event)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		if created {
+			return resultCreated
+		} else {
+			return resultOK
+		}
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) loginHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	form, err := (&Request{r}).ReadEmailAndPwd()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	user, err := web.database.LoadUserByEmailAndPassword(form.Email, form.Pwd)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+		form, err := (&Request{r}).ReadEmailAndPwd()
+		if err != nil {
+			return resultBadRequest
+		}
 
-	id, err := web.database.CreateSession(user.Id)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		user, err := web.database.LoadUserByEmailAndPassword(form.Email, form.Pwd)
+		if err != nil {
+			return resultNotFound
+		}
 
-	w.WriteHeader(http.StatusCreated)
-	w.WriteJSON(id)
+		id, err := web.database.CreateSession(user.Id)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return &webResult{Status: http.StatusCreated, JSON: id}
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) logoutHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	sessionId, err := (&Request{r}).ReadSessionId()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	err = web.database.RemoveSession(sessionId)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-	}
+		sessionId, err := (&Request{r}).ReadSessionId()
+		if err != nil {
+			return resultBadRequest
+		}
+
+		err = web.database.RemoveSession(sessionId)
+		if err != nil {
+			return resultNotFound
+		}
+
+		return resultOK
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) deleteEventHandler(w traffic.ResponseWriter, r *traffic.Request) {
 
-	user, err := web.checkSession(&Request{r})
-	if err != nil || !bson.IsObjectIdHex(r.Param("id")) {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	result := func() *webResult {
 
-	var data Event
-	event := &data
-	err = web.database.Table("event").LoadById(bson.ObjectIdHex(r.Param("id")), event)
-	if err != nil {
-		return /* http.StatusOK */
-	}
+		user, err := web.checkSession(&Request{r})
+		if err != nil || !bson.IsObjectIdHex(r.Param("id")) {
+			return resultBadRequest
+		}
 
-	if event.OrganizerId != user.Id {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		var data Event
+		event := &data
+		err = web.database.Table("event").LoadById(bson.ObjectIdHex(r.Param("id")), event)
+		if err != nil {
+			return resultOK
+		}
 
-	err = web.database.Table("event").DeleteById(event.Id)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+		if event.OrganizerId != user.Id {
+			return resultBadRequest
+		}
+
+		err = web.database.Table("event").DeleteById(event.Id)
+		if err != nil {
+			return &webResult{Status: http.StatusInternalServerError, Error: err}
+		}
+
+		return resultOK
+	}()
+
+	web.handle(w, result)
 }
 
 func (web *WebServer) Start() {
